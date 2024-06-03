e52e272c051147d0ed0142589c747e3620920429
Ferrster <lopan.vlad@yandex.ru>
2024-06-03 21:51:46 +0300

    Display name for actions

--- src/dll/include/smgm/actions/action.h
+++ src/dll/include/smgm/actions/action.h
@@ -1,18 +1,27 @@
 #pragma once
 
 #include <functional>
+#include <string>
 
 namespace smgm {
 class Action {
  public:
-  Action() = default;
   virtual bool Trigger() = 0;
+  const std::string& GetDisplayName() const noexcept { return display_name_; }
+
+ protected:
+  Action() = default;
+  Action(const std::string& name) : display_name_(name) {}
+
+  std::string display_name_;
 };
 
 class BasicAction : public Action {
  public:
   using FunctionSigT = bool();
-  BasicAction(const std::function<FunctionSigT>& action) : action(action) {}
+  BasicAction(const std::function<FunctionSigT>& action,
+              const std::string& name = "unnamed")
+      : Action(name), action(action) {}
 
   bool Trigger() override {
     if (!action) return false;
